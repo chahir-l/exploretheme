62643cb4866c2b0485e822d1e00fda296dab1c87
chahir-l <c.loukili62@gmail.com>
2020-08-25 12:17:24 +0200

    structure

--- index.php
+++ index.php
@@ -4,8 +4,21 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    
+    <header>
+        <img src="img/logo.png" alt="" />
+        <h4>Best list logos</h4>
+    </header>
+    <nav>
+        <ul>
+            <li><a href="" >Accueil</a></li>
+            <li><a href="" >Accueil</a></li>
+        </ul>
+    <nav>
+    <main>
+    </main>
+    <footer></footer>
 </body>
 </html>
